70a43aa5d5b5059c05bf32c7cb2f9c5422886910
cool_ZC <52567642+z657540196@users.noreply.github.com>
2020-11-18 23:48:18 +0800

    Add files via upload

---
+++ components/Alert.vue
@@ -0,0 +1,44 @@
+<!-- 添加新用户成功后弹窗 -->
+
+<template v-if="hide">
+	<!-- 弹窗html https://www.bootcss.com/ 组件中找到弹窗代码 贴过来即可 -->
+  	<div class="alert alert-warning alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'alert',
+  props:['message'],
+  // data(){
+  //   return {
+  //     hide:true,
+  //     timer:null
+  //   }
+  // },
+  // mounted() {
+  //   if (this.timer) {
+  //     clearInterval(this.timer)
+  //   } else{
+  //     this.timer = setInterval (() = >{
+  //       this.stopTo()
+  //     },1000)
+  //   },
+  //   stopTo(){
+  //     this.hide = false
+  //   }
+  // },
+  // destroyed(){
+  //   clearInterval(this.timer)
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ components/Customers.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="customers container">
+      <Alert v-if='alert' v-bind:message="alert"></Alert>
+      <h1 class="page-header">用户管理系统</h1>
+      <input type="text" class="from-control" placeholder="请输入搜索内容" v-model="filterInput">
+      <br>
+      <table class="table table-stripet">
+        <thead>
+          <tr >
+            <th>姓名</th>
+            <th>电话</th>
+            <th>邮箱</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        	<tr v-for="customer in filterBy(customers,filterInput)">
+        		<td>{{customer.name}}</td>
+        		<td>{{customer.phone}}</td>
+        		<td>{{customer.email}}</td>
+
+        		<td>
+              <router-link class='btn btn-default' v-bind:to="'/customer/'+customer.id">详情</router-link>
+              </td>
+        	</tr>
+        </tbody>
+
+      </table>
+    </div>
+</template>
+
+<script>
+ import Alert from './Alert'
+
+export default {
+  name: 'customers',
+  data () {
+    return {
+      customers:[],
+      alert:'',
+      filterInput:''
+    }
+  },
+  methods:{
+    fetchCustomers(){
+        this.$http.get('http://localhost:3000/users')
+            .then(function(response){
+              //  console.log(response)
+              this.customers = response.body;
+          })
+    },
+    filterBy(customers,value){
+      return customers.filter(function(customer){
+          return customer.name.match(value);
+          if(customer.name.match(value) === ''){
+            alert('没有数据')
+          }
+      })
+
+    }
+  },
+  created() {   // created 这个钩子在实例被创建之后被调用
+    if(this.$route.query.alert){
+      this.alert = this.$route.query.alert
+    }
+    this.fetchCustomers()
+  },
+  updated() {   // created 这个钩子在实例被创建之后被调用
+    this.fetchCustomers()
+  },
+  components:{
+      Alert
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
